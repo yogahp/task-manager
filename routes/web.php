7efd5b36443a6fba61e0e04553defb13480f41d4
Yoga Hapriana <thenelse.v2@gmail.com>
2024-07-04 16:39:41 +0700

    Create Controllers and Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\ProjectController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('tasks', TaskController::class);
+Route::resource('projects', ProjectController::class);
